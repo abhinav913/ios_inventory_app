9168fbdcb73d2de4bd761f429df9702dd738edac
Abhinav Shah <abhinav.shah91396@gmail.com>
2016-07-07 15:53:11 -0700

    Initial Commit

---
+++ ios_Inventory/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ios_Inventory
+//
+//  Created by Abhinav Shah on 7/7/16.
+//  Copyright © 2016 Abhinav Shah. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
