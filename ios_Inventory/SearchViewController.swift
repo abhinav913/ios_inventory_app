0dab3fc4a1740e6a2043d1be10c0aadfd4644f58
Abhinav Shah <abhinav.shah91396@gmail.com>
2016-07-08 16:12:22 -0700

    made home screen

--- ios_Inventory/SearchViewController.swift
+++ ios_Inventory/SearchViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UITableViewController {
 
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //Table View
+    override 
 
 }
 
