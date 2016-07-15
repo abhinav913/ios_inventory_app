dd4d1076d14bc0192ad52562944950b5d3dac90d
Abhinav Shah <abhinav.shah91396@gmail.com>
2016-07-15 13:14:00 -0700

    git aint workin

---
+++ ios_Inventory/ItemObject.swift
@@ -0,0 +1,19 @@
+//
+//  ItemObject.swift
+//  ios_Inventory
+//
+//  Created by Abhinav Shah on 7/9/16.
+//  Copyright © 2016 Abhinav Shah. All rights reserved.
+//
+
+import Foundation
+
+
+struct testItem {
+    let name: String
+    let category: String
+}
+
+struct categoryItem {
+    let name: String
+}
